69f98802393887aac136fe1386550014d801f97e
gwonyejun <kyjuni1059@gmail.com>
2023-07-17 15:07:02 +0900

    초기 템플릿 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,25 @@
 import React from 'react';
-import { Routes, Route, Link,useLocation  } from 'react-router-dom';
-
+import logo from './logo.svg';
 import './App.css';
-
-function Home() {
-  return <h1>Home</h1>;
-}
-
-function About() {
-  return <h1>About</h1>;
-}
 
 function App() {
   return (
-
     <div className="App">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/about" Component={About} />
-      </Routes>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-
   );
 }
 
