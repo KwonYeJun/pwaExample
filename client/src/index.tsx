f5e99f93cfffc5647316e928744f307d7e1d10ea
gwonyejun <kyjuni1059@gmail.com>
2023-07-17 15:20:34 +0900

    클릭 이벤트 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,39 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(
+      function(registration) {
+        console.log('Service Worker 등록 성공:', registration.scope);
+      },
+      function(error) {
+        console.log('Service Worker 등록 실패:', error);
+      }
+    );
+  });
+}
+async function requestNotificationPermission() {
+  const permission = await Notification.requestPermission();
+  if (permission === 'granted') {
+    console.log('알림 권한이 허용되었습니다.');
+  } else {
+    console.log('알림 권한이 거부되었습니다.');
+  }
+}
+requestNotificationPermission();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter >
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
