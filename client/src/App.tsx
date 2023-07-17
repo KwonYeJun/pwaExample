13f9660f1e5e76f54448c7a4d3c1a1d5149cc7e6
gwonyejun <kyjuni1059@gmail.com>
2023-07-17 16:17:54 +0900

    알림 설정 팝업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Routes, Route, Link,useLocation  } from 'react-router-dom';
 
 import './App.css';
@@ -13,7 +13,34 @@ function About() {
 
 function App() {
   
+  async function requestNotificationPermission() {
+    if ('Notification' in window) {
+      const permission = await Notification.requestPermission();
+      if (permission !== 'granted') {
+        console.log('알림 권한이 거부되었습니다.');
+      }
+    }
+  }
+  
+  useEffect(() => {
+    requestNotificationPermission();
+  }, []);
 
+  function handleNotificationClick() {
+    // 알림이 클릭되었을 때 수행할 동작을 정의합니다.
+    console.log('알림이 클릭되었습니다.');
+  }
+  
+  function sendNotification() {
+    if ('Notification' in window && Notification.permission === 'granted') {
+      const notification = new Notification('해킹 알림이에요🎃', {
+        body: '알림을 설정해 주세요 찡긋',
+        icon: 'path_to_icon', // 알림 아이콘 경로
+      });
+  
+      notification.addEventListener('click', handleNotificationClick);
+    }
+  }
 
   return (
 
@@ -32,6 +59,7 @@ function App() {
         <Route path="/" Component={Home} />
         <Route path="/about" Component={About} />
       </Routes>
+      <button onClick={sendNotification}>test</button>
     </div>
 
   );
