3c280da381c5dfd73350c34679b3130d062befda
gwonyejun <kyjuni1059@gmail.com>
2023-07-19 16:06:52 +0900

    전에 설정 해주었던 install코드와 가로채기 코드 합치기

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,43 +1,43 @@
-// const CACHE_NAME = 'Ripple';
-// const urlsToCache = ['/'];
+const CACHE_NAME = 'Ripple';
+const urlsToCache = ['/'];
 
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log('Opened cache');
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
-//       return fetch(event.request);
-//     })
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
 
 // service-worker.js
 
-// 서비스 워커 설치
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
-  // 필요한 리소스를 캐싱하는 로직 작성 가능
-});
+// // 서비스 워커 설치
+// self.addEventListener('install', event => {
+//   console.log('Service Worker installed');
+//   // 필요한 리소스를 캐싱하는 로직 작성 가능
+// });
 // 서비스 워커 활성화
 self.addEventListener('activate', event => {
   console.log('Service Worker activated');
   // 이전 버전의 캐시 정리 또는 업데이트 로직 작성 가능
 });
 // 네트워크 요청 가로채기
-self.addEventListener('fetch', event => {
-  console.log('Fetch intercepted:', event.request.url);
-  // 캐시에서 리소스 반환 또는 네트워크 요청 수정 가능
-});
+// self.addEventListener('fetch', event => {
+//   console.log('Fetch intercepted:', event.request.url);
+//   // 캐시에서 리소스 반환 또는 네트워크 요청 수정 가능
+// });
 // 알림 수신
 self.addEventListener('push', event => {
   console.log('Push notification received');
