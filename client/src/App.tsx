d02a985d3ad14a29a97215cf2ee0fe73e4c4cc5e
gwonyejun <kyjuni1059@gmail.com>
2023-07-19 15:35:29 +0900

    service.ts -> App.tsx 이렇게 sw연구를 진행 해본다.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,35 +13,7 @@ function About() {
 
 function App() {
   
-  async function requestNotificationPermission() {
-    if ('Notification' in window) {
-      const permission = await Notification.requestPermission();
-      if (permission !== 'granted') {
-        console.log('알림 권한이 거부되었습니다.');
-      }
-    }
-  }
   
-  useEffect(() => {
-    requestNotificationPermission();
-  }, []);
-
-  function handleNotificationClick() {
-    // 알림이 클릭되었을 때 수행할 동작을 정의합니다.
-    console.log('알림이 클릭되었습니다.');
-  }
-  
-  function sendNotification() {
-    if ('Notification' in window && Notification.permission === 'granted') {
-      const notification = new Notification('해킹 알림이에요🎃', {
-        body: '알림을 설정해 주세요 찡긋',
-        icon: 'path_to_icon', // 알림 아이콘 경로
-      });
-  
-      notification.addEventListener('click', handleNotificationClick);
-    }
-  }
-
   return (
 
     <div className="App">
@@ -59,10 +31,39 @@ function App() {
         <Route path="/" Component={Home} />
         <Route path="/about" Component={About} />
       </Routes>
-      <button onClick={sendNotification}>test</button>
+      {/* <button onClick={sendNotification}>test</button> */}
     </div>
 
   );
 }
 
 export default App;
+
+  // async function requestNotificationPermission() {
+  //   if ('Notification' in window) {
+  //     const permission = await Notification.requestPermission();
+  //     if (permission !== 'granted') {
+  //       console.log('알림 권한이 거부되었습니다.');
+  //     }
+  //   }
+  // }
+  
+  // useEffect(() => {
+  //   requestNotificationPermission();
+  // }, []);
+
+  // function handleNotificationClick() {
+  //   // 알림이 클릭되었을 때 수행할 동작을 정의합니다.
+  //   console.log('알림이 클릭되었습니다.');
+  // }
+  
+  // function sendNotification() {
+  //   if ('Notification' in window && Notification.permission === 'granted') {
+  //     const notification = new Notification('해킹 알림이에요🎃', {
+  //       body: '알림을 설정해 주세요 찡긋',
+  //       icon: 'path_to_icon', // 알림 아이콘 경로
+  //     });
+  
+  //     notification.addEventListener('click', handleNotificationClick);
+  //   }
+  // }
