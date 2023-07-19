fb8ac4a90186238b7d4227e33243f398455c91d0
gwonyejun <kyjuni1059@gmail.com>
2023-07-19 16:12:36 +0900

    중복 되는 코드를 주석 처리 해주었다.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter } from 'react-router-dom';
 
 if ('serviceWorker' in navigator) {
@@ -17,28 +17,23 @@ if ('serviceWorker' in navigator) {
     );
   });
 }
-async function requestNotificationPermission() {
-  const permission = await Notification.requestPermission();
-  if (permission === 'granted') {
-    console.log('알림 권한이 허용되었습니다.');
-  } else {
-    console.log('알림 권한이 거부되었습니다.');
-  }
-}
-requestNotificationPermission();
+// async function requestNotificationPermission() {
+//   const permission = await Notification.requestPermission();
+//   if (permission === 'granted') {
+//     console.log('알림 권한이 허용되었습니다.');
+//   } else {
+//     console.log('알림 권한이 거부되었습니다.');
+//   }
+// }
+// requestNotificationPermission();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter >
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
